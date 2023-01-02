eca7538fe1d914b6fb18b0d84209567d523da5c4
Richcy Dian Sukma <richcyveliz@gmail.com>
2023-01-02 11:52:32 +0700

    Checkpoint Study: 12:00

---
+++ DotnetBasic/Statement/Declarative.cs
@@ -0,0 +1,13 @@
+﻿public class Declarative{
+    public static void SampleDeclarative(){
+        Console.WriteLine("Declarative Test");
+
+        double area = 0.0;
+        Console.WriteLine("Area Value : " + area);
+        double radius = 2;
+        Console.WriteLine("Radius Value : " + radius);
+
+        const double pi = 3.14159;
+        Console.WriteLine("Pi Value : " + pi);
+    }
+}
