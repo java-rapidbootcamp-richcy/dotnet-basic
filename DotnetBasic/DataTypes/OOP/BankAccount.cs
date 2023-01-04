adb711a22320d19ae18501af939d7a3edcf7b8fd
Richcy Dian Sukma <richcyveliz@gmail.com>
2023-01-04 12:00:31 +0700

    Checkpoint Study: 12:00

--- DotnetBasic/DataTypes/OOP/BankAccount.cs
+++ DotnetBasic/DataTypes/OOP/BankAccount.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace DataTypes.OOP
 {
@@ -11,17 +14,113 @@ namespace DataTypes.OOP
         private static int accountNumberSeed = 1234567890;
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach(var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+        private readonly decimal _minimumBalance;
+
         public BankAccount() { }    
         
+        public BankAccount(string name, decimal initialBalance) : this(name, initialBalance, 0) { }
+      
+        public virtual void PerformMonthEndTransactions()
+        {
+
+        }
+        public BankAccount(string name, decimal initialBalance,decimal minimumBalance)
+        {
+            Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+            Owner = name;
+            _minimumBalance = minimumBalance;
+            if (initialBalance > 0)
+            {
+                MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+            }
+        }
+        /*
         public BankAccount(string name, decimal initialBalance)
         {
-            this.Owner = name;
-            this.Balance = initialBalance;
-            this.Number = accountNumberSeed.ToString();
+            Number = accountNumberSeed.ToString();
             accountNumberSeed++;
+
+            Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "initial balance");
         }
-        public void MakeDeposit(decimal amount, DateTime date, string note) { }
-        public void MakeWithdrawal(decimal amount, DateTime date, string note) { }
+        */
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            Transaction? overdraftTransaction = CheckWithdrawalLimit(Balance - amount < _minimumBalance);
+            Transaction? withdrawal = new(-amount, date, note);
+            allTransactions.Add(withdrawal);
+            if (overdraftTransaction != null)
+            {
+                allTransactions.Add(overdraftTransaction);
+            }
+        }
+        protected virtual Transaction? CheckWithdrawalLimit(bool isOverdrawn)
+        {
+            if (isOverdrawn)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            else
+            {
+                return default;
+            }
+        }
+        /*
+        public void MakeWithdrawal(decimal amount,DateTime date,string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            if (Balance-amount<0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+        */
+        public string GetAccountHistory()
+        {
+            var report = new System.Text.StringBuilder();
+
+            decimal balance = 0;
+            report.AppendLine("Date\t\tAmount\tBalance\tNote");
+            foreach(var item in allTransactions)
+            {
+                balance += item.Amount;
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+            }
+            return report.ToString();
+        }
     }
 }
